578a5e1045f934650a546b9459ce7604dccaa95a
Suryarpan <52556381+Suryarpan@users.noreply.github.com>
2021-06-18 19:33:34 +0530

    update and clean up

--- printError.py
+++ printError.py
@@ -2,14 +2,20 @@ from os import abort
 
 
 def printErr(err, level=0):
-    level_str = ''
-    if level == 1:
+    """Error logging function and aborts the program for level > 2
+
+    :param err: An error or a string
+    :type err: BaseException or str
+    :param level: Level of error, defaults to 0
+    :type level: int, optional
+    """
+    if level <= 1:
         level_str = 'NOTE: '
     elif level == 2:
         level_str = 'WARN: '
     elif level > 2:
         level_str = 'FATAL: '
 
-    print(f'VolDist: {level_str}{err.strerror}')
+    print(f'VolDist: {level_str}{err}')
     if level > 2:
         abort()
