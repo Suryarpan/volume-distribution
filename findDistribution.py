7a8248805caac3ccb1295fcac5d56f669b9276eb
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2021-06-19 19:37:00 +0530

    implimented calculation of volume distribution

---
+++ findDistribution.py
@@ -0,0 +1,33 @@
+import numpy as np
+from scipy import stats
+from scipy import integrate
+
+def findDistribution(DoseData, ConcData, TimeData):
+    """Finds the Volume of distribution
+
+    :param DoseData: Dose administered
+    :type DoseData: numpy.ndarray
+    :param ConcData: Concentration of drug is plasma
+    :type ConcData: numpy.ndarray
+    :param TimeData: Time
+    :type TimeData: numpy.ndarray
+    :return: Areal volume distribution, Steady state volume distribution
+    :rtype: float
+    """
+    # Manipulating Necessary Parameters
+    dose = DoseData[0]
+    lnConcData = np.log(ConcData)
+    ConcTimeData = ConcData * TimeData
+    res = stats.linregress(TimeData, lnConcData)
+    k_el = res.slope
+    areaConcTime = integrate.trapezoid(ConcData, TimeData)
+    areaConcTimeTime = integrate.trapezoid(ConcTimeData, TimeData)
+    meanResidenceTime = areaConcTimeTime/areaConcTime
+
+    # Calculating Volume Distribution
+    vArea = (dose)/(k_el * areaConcTime)
+    vSs = ((dose)/(areaConcTime)) * meanResidenceTime
+
+    return vArea, vSs, k_el
+
+
