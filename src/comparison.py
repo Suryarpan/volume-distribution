4330cd0ef623377e63eaf43739472f1a2eaa37af
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2021-06-20 23:08:21 +0530

    Updated Implimentation

--- src/comparison.py
+++ src/comparison.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from printError import printErr
 
@@ -21,17 +22,18 @@ def reader(FileName):
         printErr('File type not compatible', 3)
 
 
-def comparison(FileName, VolumeDistribution):
+def comparison(FileName, VolumeDistribution, outFolder):
     """Produces a sorted .xlsx file, where one can compare the volume distribution of sample with some known drugs
 
     :param FileName: Name/Path of dataset containg name and volume distribution data of known drugs
     :type FileName: str
     :param VolumeDistribution: volume distribution of the sample
     :type VolumeDistribution: float
+    :param outFolder: Folder to save all of output data (graphs, comparison data, computational result)
+    :type outFolder: str
     """
     # Creating data heads
     nameHead = ['Name', 'NAME', 'name']
-    # volumeHead = ['Volume_Distribution', 'VOLUME_DISTRIBUTION', 'volume_distribution', 'V_d', 'V']
 
     # Reading data from file
     df1 = reader(FileName)
@@ -50,7 +52,9 @@ def comparison(FileName, VolumeDistribution):
 
     df1 = df1.append(df2, ignore_index=True)
 
+    outFile = os.path.join(outFolder, 'Drug_Comparison.xlsx')
+
     # Sorting the data and exporting it
     sorted_df = df1.sort_values(by=['V'], ascending=True)
-    sorted_df.to_excel('Comparison_Dataset.xlsx',
+    sorted_df.to_excel(outFile,
                        sheet_name='Comparison Sheet')
